b9c7416c0fef444fa27981ebe2e0a1fa08e69f01
Mandeep-Singh <mandeep.dev1309@gmail.com>
2024-06-23 15:41:28 +0530

    js-test

---
+++ my_pro/test.js
@@ -0,0 +1,3 @@
+document.getElementById("opopo").textContent ="Lorem ipsLorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente laborum illo excepturi dignissimos architecto veritatis? Nulla placeat cupiditate amet, reprehenderit dolorum molestias eum numquam, debitis veritatis fugit magni quidem nam? ";
+
+
